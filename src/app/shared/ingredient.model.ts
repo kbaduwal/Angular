f814b01a217c65a614dce7afa82379e4c61daafa
kbaduwal <khakendra1999@gmail.com>
2023-05-15 22:43:17 +0530

    Button for add delete clear added

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient{
+/*    public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number){
+        this.name = name;
+        this.amount = amount;
+    
+    }*/
+
+    //Above thing can be written as
+    constructor(public name:string, public amount:number){}
+
+    
+}
